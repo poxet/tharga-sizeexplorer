98556675ebb79c6eadb1d07106110068f15dec78
Daniel Bohlin <daniel.bohlin@gmail.com>
2016-06-19 21:21:55 +0200

    Prepare for manual deploy

--- Setup/ChocolateyInstall.ps1
+++ Setup/ChocolateyInstall.ps1
@@ -1,6 +1,6 @@
 $packageName = 'windirstat'
 $fileType = 'msi'
-$url = 'http://www.thargelion.net/Resources/#Environment#/Tharga.SizeExplorer.#Version#.msi'
+$url = 'http://www.thargelion.net/Resources/Release/Tharga.SizeExplorer.1.0.0.msi'
 $silentArgs = '/q'
 
 Install-ChocolateyPackage $packageName $fileType "$silentArgs" "$url"
