1162295f3192201e961ce6078e64ea7699e188db
Daniel Bohlin <daniel.bohlin@gmail.com>
2016-06-22 01:25:03 +0200

    Updated names and paths to match chocolaty policy.

--- Setup/ChocolateyInstall.ps1
+++ Setup/ChocolateyInstall.ps1
@@ -1,6 +1,6 @@
 $packageName = 'windirstat'
 $fileType = 'msi'
-$url = 'http://www.thargelion.net/Resources/Release/Tharga.SizeExplorer.1.0.0.msi'
+$url = 'http://www.thargelion.net/Resources/Release/Tharga.SizeExplorer.1.0.1.msi'
 $silentArgs = '/q'
 
 Install-ChocolateyPackage $packageName $fileType "$silentArgs" "$url"
